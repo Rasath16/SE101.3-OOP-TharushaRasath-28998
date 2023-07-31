2430ffe323fb6363051425e49ae00ccea1304b52
Rasath16 <135937167+Rasath16@users.noreply.github.com>
2023-07-31 19:08:20 +0530

    Add files via upload

---
+++ lab1/question2/Nd.java
@@ -0,0 +1,8 @@
+public class Nd {
+
+    public static void main(String[] args) {
+               System.out.println("Tharusha Rasath");
+        System.out.println("BSc (Hons) in Computer Science");
+
+    }
+}
